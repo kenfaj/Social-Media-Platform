9f6aa4793e71629ce288417331a5bc989fa37673
kenfaj <149665960+kenfaj@users.noreply.github.com>
2025-02-24 16:34:53 +0800

    Added MySQLCREDENTIALS, only change this for your own configurations

---
+++ WebApplicationDB/src/main/java/com/mycompany/webapplicationdb/model/MySQLCredentials.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.webapplicationdb.model;
+
+/**
+ *
+ * @author ken
+ */
+public class MySQLCredentials {
+    public static final String DEFAULT_SERVERHOST = "localhost";
+    public static final String DEFAULT_DATABASE = "mp2";
+    public static final String DEFAULT_PORT = "3306";
+    public static final String DEFAULT_USERNAME = "root";
+    public static final String DEFAULT_PASSWORD= "1234";
+}
