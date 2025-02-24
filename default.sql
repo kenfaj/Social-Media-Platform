10ba7ece50807600c39cd2c5161304d77c22e93a
ken <142144694+kenDRoger@users.noreply.github.com>
2025-02-24 14:55:25 +0800

    added table posts in default.sql

--- default.sql
+++ default.sql
@@ -1,1 +1,34 @@
-create database MP2;
+drop database if exists mp2;
+
+create database mp2;
+
+use mp2;
+
+create table account(
+    username VARCHAR(30) primary key,
+    password VARCHAR(30),
+    user_role text check (user_role in ('admin','super_admin', 'guest'))
+);
+
+create table post(
+    title VARCHAR(30),
+    content text(200),
+    id int auto_increment primary key
+);
+
+create table posts(
+    user_name VARCHAR(30),
+    post1 int,
+    post2 int,
+    post3 int,
+    post4 int,
+    post5 int,
+    foreign key (user_name) references account(username),
+    foreign key (post1) references post(id),
+    foreign key (post2) references post(id),
+    foreign key (post3) references post(id),
+    foreign key (post4) references post(id),
+    foreign key (post5) references post(id)
+);
+
+
