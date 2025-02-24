d821b8a38bf7b5649dc9b8f260fe9027f9aedae4
ken <142144694+kenDRoger@users.noreply.github.com>
2025-02-24 19:25:11 +0800

    Added getCredentials and getUserRole and renewConnection method in JDBCModel

--- WebApplicationDB/src/main/java/com/mycompany/webapplicationdb/model/JDBCModel.java
+++ WebApplicationDB/src/main/java/com/mycompany/webapplicationdb/model/JDBCModel.java
@@ -2,7 +2,11 @@ package com.mycompany.webapplicationdb.model;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.HashMap;
+import java.util.Map;
 
 import static com.mycompany.webapplicationdb.model.MySQLCredentials.DEFAULT_PASSWORD;
 import static com.mycompany.webapplicationdb.model.MySQLCredentials.DEFAULT_PORT;
@@ -37,13 +41,52 @@ public class JDBCModel {
             System.out.println("Failed Databse Connection");
             e.printStackTrace();
         }
-
-         
     }
 
     public JDBCModel() {
         this(DATABASE);
     }
+
+
+    // Method to get a HashMap of Usernames and passwords from Connection
+    public Map<String, String> getCredentials() {
+        Map<String, String> credentials = new HashMap<>();
+        String query = "SELECT username, password FROM account";
+
+        try (Statement stmt = conn.createStatement();
+             ResultSet rs = stmt.executeQuery(query)) {
+
+            while (rs.next()) {
+                String username = rs.getString("username");
+                String password = rs.getString("password");
+                credentials.put(username, password);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return credentials;
+    }
+
+
+    // Method to get the user_role using username from Connection
+    public String getUserRole(String username) {
+        String role = "";
+        String query = "SELECT user_role FROM account WHERE username = '" + username + "'";
+
+        try (Statement stmt = conn.createStatement();
+             ResultSet rs = stmt.executeQuery(query)) {
+
+            while (rs.next()) {
+                role = rs.getString("user_role");
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return role;
+    }
+
 
     public static void main(String[] args) {
         new JDBCModel("mp2");
