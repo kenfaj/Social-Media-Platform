def52409f5def2d9ebcd943cecba38d0ab3c088f
kenfaj <149665960+kenfaj@users.noreply.github.com>
2025-02-24 20:29:30 +0800

    Initial LoginServlet, Used renewConnection Method, Adjusted web.xml

--- WebApplicationDB/src/main/java/com/mycompany/webapplicationdb/model/JDBCModel.java
+++ WebApplicationDB/src/main/java/com/mycompany/webapplicationdb/model/JDBCModel.java
@@ -14,7 +14,11 @@ import static com.mycompany.webapplicationdb.model.MySQLCredentials.DEFAULT_SERV
 import static com.mycompany.webapplicationdb.model.MySQLCredentials.DEFAULT_USERNAME;
 
 public class JDBCModel {
+
     private Connection conn;
+    private String jdbcUrl;
+    private String userName;
+    private String password;
 
     // Default dont change, if you want to change values for your database change in
     // MySQLCredentials
@@ -28,33 +32,41 @@ public class JDBCModel {
         String serverHost = SERVERHOST.isEmpty() ? DEFAULT_SERVERHOST : SERVERHOST;
         String port = PORT.isEmpty() ? DEFAULT_PORT : PORT;
         String databaseName = database.isEmpty() ? DATABASE : database;
-        String userName = USERNAME.isEmpty() ? DEFAULT_USERNAME : USERNAME;
-        String password = PASSWORD.isEmpty() ? DEFAULT_PASSWORD : PASSWORD;
-        
-        String jdbcUrl = "jdbc:mysql://" + serverHost + ":" + port + "/" + databaseName + "?useSSL=false&allowPublicKeyRetrieval=true&serverTimezone=UTC";
-
-        try {
-            conn = DriverManager.getConnection(jdbcUrl, userName, password);
-            //tester
-            System.out.println("Successful Databse Connection using: " + jdbcUrl);
-        } catch (SQLException e) {
-            System.out.println("Failed Databse Connection");
-            e.printStackTrace();
-        }
+        userName = USERNAME.isEmpty() ? DEFAULT_USERNAME : USERNAME;
+        password = PASSWORD.isEmpty() ? DEFAULT_PASSWORD : PASSWORD;
+        jdbcUrl = "jdbc:mysql://" + serverHost + ":" + port + "/" + databaseName
+                + "?useSSL=false&allowPublicKeyRetrieval=true&serverTimezone=UTC";
+        conn = renewConnection();
     }
 
     public JDBCModel() {
         this(DATABASE);
     }
 
+    // method to renew connection
+    private Connection renewConnection() {
+        try {
+            if (conn != null) {
+                conn.close();
+            }
+            Connection c = DriverManager.getConnection(jdbcUrl, userName, password);
+            // tester
+            System.out.println("Successful Databse Connection using: " + jdbcUrl);
+            return c;
+        } catch (SQLException e) {
+            System.out.println("Failed Databse Connection");
+            e.printStackTrace();
+        }
+        throw new UnsupportedOperationException();
+    }
 
     // Method to get a HashMap of Usernames and passwords from Connection
     public Map<String, String> getCredentials() {
+        conn = renewConnection();
         Map<String, String> credentials = new HashMap<>();
         String query = "SELECT username, password FROM account";
 
-        try (Statement stmt = conn.createStatement();
-             ResultSet rs = stmt.executeQuery(query)) {
+        try ( Statement stmt = conn.createStatement();  ResultSet rs = stmt.executeQuery(query)) {
 
             while (rs.next()) {
                 String username = rs.getString("username");
@@ -68,14 +80,13 @@ public class JDBCModel {
         return credentials;
     }
 
-
     // Method to get the user_role using username from Connection
     public String getUserRole(String username) {
+        conn = renewConnection();
         String role = "";
         String query = "SELECT user_role FROM account WHERE username = '" + username + "'";
 
-        try (Statement stmt = conn.createStatement();
-             ResultSet rs = stmt.executeQuery(query)) {
+        try ( Statement stmt = conn.createStatement();  ResultSet rs = stmt.executeQuery(query)) {
 
             while (rs.next()) {
                 role = rs.getString("user_role");
@@ -86,7 +97,6 @@ public class JDBCModel {
 
         return role;
     }
-
 
     public static void main(String[] args) {
         new JDBCModel("mp2");
