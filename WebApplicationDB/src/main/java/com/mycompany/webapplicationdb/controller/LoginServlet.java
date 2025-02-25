d97490bc54b5d0967e657c05d8979bd0849142de
kenfaj <149665960+kenfaj@users.noreply.github.com>
2025-02-25 14:17:11 +0800

    Fixes LoginServlet

--- WebApplicationDB/src/main/java/com/mycompany/webapplicationdb/controller/LoginServlet.java
+++ WebApplicationDB/src/main/java/com/mycompany/webapplicationdb/controller/LoginServlet.java
@@ -25,33 +25,27 @@ import com.mycompany.webapplicationdb.model.User;
  *
  * @author ken
  */
-@WebServlet(name = "LoginServlet", urlPatterns = { "/LoginServlet" })
+@WebServlet(name = "LoginServlet", urlPatterns = {"/LoginServlet"})
 public class LoginServlet extends HttpServlet {
 
-    /**
-     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
-     * methods.
-     *
-     * @param request  servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException      if an I/O error occurs
-     */
-    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+    //New method for processrequest to handle the exceptions
+    protected void processRequestException(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException, DatabaseConnectionFailedException {
         response.setContentType("text/html;charset=UTF-8");
 
+        //1. GET PARAMETERS
         // get the username and password from the form
         String username = request.getParameter("username");
         String password = request.getParameter("password");
 
+        //2. GET DATABASE DATA
         // get username and password from database
         JDBCModel model;
         Map<String, String> map = new HashMap<>();
         String userRole = "";
 
+        //3. HANDLE UNEXPECTED ACCESS(Session)
         // check if session object has attribute username
-
         HttpSession session = request.getSession(false);
         if (session == null || session.getAttribute("username") == null) {
             // TODO: handle unexpected access(siguro check lang if nakalogin as user from
@@ -59,17 +53,20 @@ public class LoginServlet extends HttpServlet {
 
         }
         
+        //4. INITIALIZE MODELS
+        try {
+            model = new JDBCModel(MySQLCredentials.DEFAULT_DATABASE);
+            map = model.getCredentials();
+            userRole = model.getUserRole(username);
+        } catch (DatabaseConnectionFailedException ex) {
+            // TODO: handle exception(web.xml then add an error page)
+            request.setAttribute("error", "Database connection failed");
+            request.getRequestDispatcher("/error.jsp").forward(request, response);
+        }
+
+        //5. SERVLET LOGIC
         // check if session object has attribute username(if user is already logged in)
         if (session.getAttribute("username") == null) {
-            try {
-                model = new JDBCModel(MySQLCredentials.DEFAULT_DATABASE);
-                map = model.getCredentials();
-                userRole = model.getUserRole(username);
-            } catch (DatabaseConnectionFailedException ex) {
-                // TODO: handle exception(web.xml then add an error page)
-                request.setAttribute("error", "Database connection failed");
-                request.getRequestDispatcher("/error.jsp").forward(request, response);
-            }
 
             // check if username exists
             if (!map.containsKey(username)) {
@@ -88,13 +85,14 @@ public class LoginServlet extends HttpServlet {
             session.setAttribute("username", username);
             session.setAttribute("user_role", userRole);
         }
-        
+
         //TESTER
-        System.out.println("username:"+username);
-        System.out.println("pass:"+password);
-        System.out.println("role:"+userRole);
+        System.out.println("username:" + username);
+        System.out.println("pass:" + password);
+        System.out.println("role:" + userRole);
         System.out.println("TESTST");
 
+        //6. REDIRECT LOGIC
         // check if user is guest
         if (userRole.equals("guest")) {
             // TODO: set request attribute for landing page
@@ -103,7 +101,8 @@ public class LoginServlet extends HttpServlet {
             request.getRequestDispatcher("landing.jsp").forward(request, response);
             return;
         }
-        
+
+        //7. SEND DATA THROUGH ATTRIBUTES
         // get list of users
         JDBCModel model2;
         model2 = new JDBCModel(MySQLCredentials.DEFAULT_DATABASE);
@@ -111,14 +110,11 @@ public class LoginServlet extends HttpServlet {
         for (User user : model2.getAccountsByRole("guest")) {
             guests.addUser(user);
         }
-
         // set request attribute for admin.jsp
         request.setAttribute("guests", guests);
-        
 
         // check if user is admin
         if (userRole.equals("admin")) {
-
             // forward to admin page
             request.getRequestDispatcher("admin/admin.jsp").forward(request, response);
             return;
@@ -142,14 +138,22 @@ public class LoginServlet extends HttpServlet {
             request.getRequestDispatcher("admin/admin.jsp").forward(request, response);
             return;
         }
-        
-        
-    }
 
-    protected void processRequestException(HttpServletRequest request, HttpServletResponse response)
+    }
+    
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         try {
-            processRequest(request, response);
+            processRequestException(request, response);
         } catch (DatabaseConnectionFailedException ex) {
             request.setAttribute("error", "Database connection failed - processRequestException");
             request.getRequestDispatcher("/error.jsp").forward(request, response);
@@ -161,29 +165,29 @@ public class LoginServlet extends HttpServlet {
     /**
      * Handles the HTTP <code>GET</code> method.
      *
-     * @param request  servlet request
+     * @param request servlet request
      * @param response servlet response
      * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException      if an I/O error occurs
+     * @throws IOException if an I/O error occurs
      */
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        processRequestException(request, response);
+        processRequest(request, response);
     }
 
     /**
      * Handles the HTTP <code>POST</code> method.
      *
-     * @param request  servlet request
+     * @param request servlet request
      * @param response servlet response
      * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException      if an I/O error occurs
+     * @throws IOException if an I/O error occurs
      */
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        processRequestException(request, response);
+        processRequest(request, response);
     }
 
     /**
